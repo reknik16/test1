5aa29ec0af04b8816bddf5dbc1a668976f2447f8
reknik16 <103887457+reknik16@users.noreply.github.com>
2024-11-25 23:34:20 +0300

    create

---
+++ main.py
@@ -0,0 +1,36 @@
+import cmath
+
+
+def find_roots(a, b, c):
+    # Вычисляет корни уравнения вида: a * x^2 + b * x + c = 0.
+    # Проверка на корректность коэффициентов
+    if a == 0:
+        raise ValueError("Первый коэффициент не может быть равен нулю для квадратного уравнения")
+
+    if not all(isinstance(coef, (int, float)) for coef in (a, b, c)):
+        raise TypeError("Коэффициенты должны быть числами")
+
+    discriminant = b ** 2 - 4 * a * c
+
+    sqrt_disc = cmath.sqrt(discriminant)
+    denominator = 2 * a
+
+    if discriminant > 0:
+        root1 = (-b + sqrt_disc) / denominator
+        root2 = (-b - sqrt_disc) / denominator
+        return (root1, root2)
+    elif discriminant == 0:
+        root = -b / denominator
+        return (root)
+    else:
+        root1 = (-b + sqrt_disc) / denominator
+        root2 = (-b - sqrt_disc) / denominator
+        return (root1, root2)
+
+# Пример использования
+a = 1
+b = 2
+c = 3
+
+result = find_roots(a, b, c)
+print("Корни уравнения:", result)
---
+++ test.py
@@ -0,0 +1,36 @@
+import unittest
+from cmath import isclose
+from main import find_roots
+
+class TestFindRoots(unittest.TestCase):
+    def test_two_real_roots(self):
+        root1, root2 = find_roots(1, -5, 6)
+        self.assertEqual(root1, 3.0)
+        self.assertEqual(root2, 2.0)
+
+    def test_one_real_root(self):
+        root = find_roots(1, -4, 4)
+        self.assertEqual(root, 2.0)
+
+    def test_complex_roots(self):
+        root1, root2 = find_roots(1, -2, 5)
+        self.assertEqual(root1, complex(1, 2))
+        self.assertEqual(root2, complex(1, -2))
+
+    def test_invalid_a(self):
+        with self.assertRaises(ValueError) as context:
+            find_roots(0, 2, 3)
+        self.assertEqual(str(context.exception), "Первый коэффициент не может быть равен нулю для квадратного уравнения")
+
+    def test_invalid_types(self):
+        with self.assertRaises(TypeError) as context:
+            find_roots("aaaaaaaaaa", 2, 3)
+        self.assertEqual(str(context.exception), "Коэффициенты должны быть числами")
+
+
+    def test_large_numbers(self):
+        roots = find_roots(9000000, 800000, 700000)
+        self.assertTrue(all(isinstance(root, complex) for root in roots))
+
+if __name__ == "__main__":
+    unittest.main()
